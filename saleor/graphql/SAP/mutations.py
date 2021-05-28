4c37fb04ec38aa5ab075c9a02b30c8d208d490f3
rrustvold <rrustvold@compustar.com>
2021-05-27 17:16:26 -0700

    Allow for bulk address creation

--- saleor/graphql/SAP/mutations.py
+++ saleor/graphql/SAP/mutations.py
@@ -18,14 +18,13 @@ from saleor.graphql.SAP.types import (
 )
 
 
-class GetBusinessPartnerFromCardCodeMixin:
+class GetBusinessPartnerMixin:
 
     @classmethod
     def get_business_partner(cls, data: dict, info):
         """Gets a business partner model object from either the base-64 encoded
         business partner id, or from an SAP card code. If both are provided, this will
         default to using the business partner ID."""
-        print(f"ALL THE DATA {data}")
         if business_partner_id := data.get("business_partner_id"):
             business_partner = cls.get_node_or_error(
                 info,
@@ -45,7 +44,6 @@ class GetBusinessPartnerFromCardCodeMixin:
 
 class BusinessPartnerCreateInput(graphene.InputObjectType):
     addresses = graphene.List(of_type=graphene.ID)
-    address_objects = graphene.List(of_type=AddressInput)
     account_balance = Decimal()
     account_is_active = graphene.Boolean()
     account_purchasing_restricted = graphene.Boolean()
@@ -68,7 +66,7 @@ class BusinessPartnerCreateInput(graphene.InputObjectType):
     warranty_preference = graphene.String()
 
 
-class MigrateBusinessPartner(ModelMutation):
+class MigrateBusinessPartner(ModelMutation, GetBusinessPartnerMixin):
     """Mutation for creating (i.e. migrating over) a business partner from SAP. If the
     id argument is passed, then this will update the existing business partner with that
     id."""
@@ -98,21 +96,8 @@ class MigrateBusinessPartner(ModelMutation):
         is supplied in the input data. This allows us to treat this mutation as a create
         or update based on the SAP card code which is unique.
         """
-
-        object_id = data.get("id")
-        qs = data.get("qs")
-        sap_bp_code = data.get("input", {}).get("sap_bp_code")
-        if object_id:
-            model_type = cls.get_type_for_model()
-            instance = cls.get_node_or_error(
-                info, object_id, only_type=model_type, qs=qs
-            )
-        elif sap_bp_code:
-            try:
-                instance = models.BusinessPartner.objects.get(sap_bp_code=sap_bp_code)
-            except models.BusinessPartner.DoesNotExist:
-                instance = cls._meta.model()
-        else:
+        instance = cls.get_business_partner(data, info)
+        if not instance:
             instance = cls._meta.model()
 
         return instance
@@ -120,17 +105,18 @@ class MigrateBusinessPartner(ModelMutation):
     @classmethod
     def clean_input(cls, info, instance, data, input_cls=None):
         """To avoid needing to make multiple queries with SAP integration framework,
-        we're going to step in front of this method and try to resolve channel names
-        and such into their IDs.
+        we're going to  resolve channel names into their django objects so that we don't
+        have to look up what their base 64 id is
         """
-        print(f"HERES THE DATA{data}")
-        address_objects = data.pop("address_objects", None)
-        channel_name = data.pop("channel_name", None)
+        cleaned_input = super().clean_input(info, instance, data, input_cls=input_cls)
+        if channel_name := data.pop("channel_name", None):
+            channel = models.Channel.objects.filter(name=channel_name).first()
+            cleaned_input["channel"] = channel
 
-        return super().clean_input(info, instance, data, input_cls=input_cls)
+        return cleaned_input
 
 
-class BusinessPartnerAddressCreate(ModelMutation, GetBusinessPartnerFromCardCodeMixin):
+class BusinessPartnerAddressCreate(ModelMutation, GetBusinessPartnerMixin):
     business_partner = graphene.Field(
         BusinessPartner,
         description="A business partner instance for which the address was created."
@@ -161,13 +147,48 @@ class BusinessPartnerAddressCreate(ModelMutation, GetBusinessPartnerFromCardCode
         error_type_field = "account_errors"
 
     @classmethod
+    def get_instance(cls, info, business_partner, **data):
+        address_type = data.get("type")
+        company_name = data.get("input", {}).get("company_name")
+
+        # Check to see if this address already exists
+        # The SAP database doesn't keep a unique id for addresses, but an index on the
+        # CRD1 table make the `Address`, `AdresType`, and `CardCode` fields
+        # unique together which we can use to prevent duplicates from being created
+        existing_address = models.Address.objects.filter(
+            businesspartneraddresses__business_partner=business_partner,
+            company_name=company_name,
+            businesspartneraddresses__type=address_type
+        ).first()
+
+        if existing_address:
+            return existing_address, True
+        else:
+            return cls._meta.model(), False
+
+    @classmethod
     def perform_mutation(cls, root, info, **data):
         address_type = data.get("type", None)
-        business_partner = cls.get_business_partner(data, info)
-        response = super().perform_mutation(root, info, **data)
-        if not response.errors:
-            business_partner.addresses.add(response.address)
+        business_partner: models.BusinessPartner = cls.get_business_partner(data, info)
+        instance, is_existing = cls.get_instance(info, business_partner, **data)
+        data = data.get("input")
+        cleaned_input = cls.clean_input(info, instance, data, input_cls=AddressInput)
+        instance = cls.construct_instance(instance, cleaned_input)
+        cls.clean_instance(info, instance)
+        cls.save(info, instance, cleaned_input)
+        cls._save_m2m(info, instance, cleaned_input)
+        cls.post_save_action(info, instance, cleaned_input)
+        response = cls.success_response(instance)
+
+        # If this is a new address, add it to the business partner
+        if not is_existing:
+            models.BusinessPartnerAddresses.objects.create(
+                business_partner_id=business_partner.id,
+                type=address_type,
+                address_id=instance.id
+            )
             response.business_partner = business_partner
+            # TODO: when should we change defaults?
             if address_type:
                 if address_type == AddressType.BILLING:
                     business_partner.default_billing_address = response.address
@@ -175,6 +196,76 @@ class BusinessPartnerAddressCreate(ModelMutation, GetBusinessPartnerFromCardCode
                     business_partner.default_shipping_address = response.address
                 business_partner.save()
         return response
+
+
+class BulkAddressInput(graphene.InputObjectType):
+    type = AddressTypeEnum(
+        required=False,
+        description=(
+            "A type of address. If provided, the new address will be "
+            "automatically assigned as the business partner's default address "
+            "of that type."
+        ),
+    )
+    input = AddressInput(
+        description="Fields required to create address.", required=True
+    )
+    business_partner_id = graphene.ID(
+        description="ID of a business partner to create address for.",
+    )
+    sap_bp_code = graphene.String(description="Create Address for Card Code")
+
+
+class BulkBusinessPartnerAddressCreate(BusinessPartnerAddressCreate):
+    """Whenever a business partner (aka card-code) is updated in SAP it will trigger
+    an event in the integration framework. We don't have a way of knowing what changed
+    about the business partner. That means it's possible that existing addresses have
+    been updated, new addresses created, or old addresses removed. It's also possible
+    that addresses haven't been changed at all, but there's no way to know. So to work
+    around that the integration framework is going to call this mutation to upsert all
+    addresses for a business partner each time. This mutation will create, update, and
+    remove from saleor accordingly."""
+
+    class Arguments:
+        input = graphene.List(of_type=BulkAddressInput)
+        business_partner_id = graphene.ID(
+            description="ID of a business partner to create address for.",
+        )
+        sap_bp_code = graphene.String(description="Create Address for Card Code")
+
+    class Meta:
+        description = "Creates many business partner addresses."
+        model = models.Address
+        permissions = (AccountPermissions.MANAGE_USERS,)
+        error_type_class = AccountError
+        error_type_field = "account_errors"
+
+    @classmethod
+    def perform_mutation(cls, root, info, **data):
+        address_inputs = data.get("input")
+        business_partner = cls.get_business_partner(data, info)
+
+        existing_addresses = set(models.BusinessPartnerAddresses.objects.filter(
+            business_partner=business_partner
+        ).values_list("address__company_name", "type"))
+
+        responses = []
+        upserted_addresses = set()
+        for address in address_inputs:
+            response = super().perform_mutation(root, info, **address)
+            responses.append(response)
+            upserted_addresses.add((address["input"]["company_name"], address["type"]))
+
+        # Remove any addresses that weren't included in the mutation
+        addresses_to_delete = existing_addresses - upserted_addresses
+        for company_name, address_type in addresses_to_delete:
+            models.Address.objects.filter(
+                businesspartneraddresses__business_partner=business_partner,
+                businesspartneraddresses__type=address_type,
+                company_name=company_name,
+            ).delete()
+
+        return cls(**{cls._meta.return_field_name: responses, "errors": []})
 
 
 class DroneRewardsCreateInput(graphene.InputObjectType):
@@ -251,7 +342,7 @@ class SAPApprovedBrandsInput(graphene.InputObjectType):
     replacements = graphene.Boolean()
 
 
-class AssignApprovedBrands(ModelMutation, GetBusinessPartnerFromCardCodeMixin):
+class AssignApprovedBrands(ModelMutation, GetBusinessPartnerMixin):
     """Mutation for assigning approved brands to a business partner"""
     approved_brands = graphene.Field(
         SAPApprovedBrands,
