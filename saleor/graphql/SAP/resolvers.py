77b90aa8ccdcdf836dc746b410aa68103f75b7ba
rrustvold <rrustvold@compustar.com>
2021-10-29 12:08:34 -0700

    Add exception handling for "apps"

--- saleor/graphql/SAP/resolvers.py
+++ saleor/graphql/SAP/resolvers.py
@@ -1,3 +1,5 @@
+from typing import Union, TYPE_CHECKING
+
 from django.db.models import Q
 import graphene
 
@@ -7,8 +9,12 @@ from saleor.core.exceptions import PermissionDenied
 from saleor.graphql.core.validators import validate_one_of_args_is_in_query
 from saleor.graphql.utils import get_user_or_app_from_context
 
+if TYPE_CHECKING:
+    from saleor.account.models import User, App
 
-def filter_business_partner_by_view_permissions(business_partner_qs, requester):
+def filter_business_partner_by_view_permissions(
+        business_partner_qs: "queryset", requester: Union[User, App]
+):
     """Given a queryset of BusinessPartner, and a requesting user, filter and return
     the queryset to only contain business partners that the user has permission to view
     """
@@ -17,18 +23,22 @@ def filter_business_partner_by_view_permissions(business_partner_qs, requester):
     elif requester.has_perm(SAPCustomerPermissions.VIEW_PROFILE):
         # Non-staff can only see business partners they are attached to either as a
         # contact or a sales rep
-        users_bps = requester.sapuserprofile.business_partners.values_list(
-            "id", flat=True
-        )
-        all_business_partner_connections = Q(
-            Q(id__in=users_bps) |
-            Q(inside_sales_rep=requester) |
-            Q(sales_manager=requester) |
-            Q(outsidesalesrep__user=requester)
-        )
-        return business_partner_qs.filter(all_business_partner_connections)
-    else:
-        return business_partner_qs.none()
+        try:
+            users_bps = requester.sapuserprofile.business_partners.values_list(
+                "id", flat=True
+            )
+        except (models.SAPUserProfile.DoesNotExist, AttributeError):
+            pass
+        else:
+            all_business_partner_connections = Q(
+                Q(id__in=users_bps) |
+                Q(inside_sales_rep=requester) |
+                Q(sales_manager=requester) |
+                Q(outsidesalesrep__user=requester)
+            )
+            return business_partner_qs.filter(all_business_partner_connections)
+
+    return business_partner_qs.none()
 
 
 def resolve_business_partner(_root, info, id=None, sapBpCode=None, **kwargs):
